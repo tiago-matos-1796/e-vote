164ac89246e0a2fa56ab0fdd022cd092e1905ac8
Tiago <tiagomatos500@hotmail.com>
2023-04-14 00:33:47 +0100

    Implement authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const config = process.env;
+
+function verifyToken(req, res, next) {
+    const token = req.body.token || req.query.token || req.headers["x-api-key"];
+
+    if(!token) {
+        return res.status(403).send("Authentication required");
+    }
+    try {
+        req.user = jwt.verify(token, config.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).send("Invalid Token");
+    }
+    return next();
+}
+
+module.exports = verifyToken;
