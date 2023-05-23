a1adc7b5d6071402b619436e1dc42968b315d2e3
Tiago <tiagomatos500@hotmail.com>
2023-05-23 03:54:25 +0100

    Add validations to methods. Add access control to routes

--- routes/statistics.route.js
+++ routes/statistics.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth.middleware');
+const {access} = require("../middleware/permission.middleware");
 
 module.exports = app => {
     const statisticsController = require("../controllers/statistics.controller");
     router.post('/:id', auth, statisticsController.vote);
-    router.get('/:id', auth, statisticsController.showResults);
+    router.get('/:id', auth, access(["MANAGER"]), statisticsController.showResults);
     app.use('/vote', router);
 }
