3ed7d58814aa3be906f745cb90e15f799ae9e469
Tiago <tiagomatos500@hotmail.com>
2023-08-11 00:28:55 +0100

    Encrypt KMS to API requests

--- services/encryption.service.js
+++ services/encryption.service.js
@@ -115,6 +115,28 @@ function internalDecrypt(data) {
   return decryptedData;
 }
 
+function KMSEncrypt(data) {
+  const cipher = crypto.createCipheriv(
+    internal_algorithm,
+    process.env.KMS_AES_KEY,
+    Buffer.from(process.env.KMS_AES_IV, "base64")
+  );
+  let encryptedData = cipher.update(data, "utf8", "base64");
+  encryptedData += cipher.final("base64");
+  return encryptedData;
+}
+
+function KMSDecrypt(data) {
+  const decipher = crypto.createDecipheriv(
+    internal_algorithm,
+    process.env.KMS_AES_KEY,
+    Buffer.from(process.env.KMS_AES_IV, "base64")
+  );
+  let decryptedData = decipher.update(data, "base64", "utf8");
+  decryptedData += decipher.final("utf8");
+  return decryptedData;
+}
+
 module.exports = {
   generateKeys,
   decrypt,
@@ -124,4 +146,6 @@ module.exports = {
   verify,
   internalEncrypt,
   internalDecrypt,
+  KMSEncrypt,
+  KMSDecrypt,
 };
--- utils/kms.utils.js
+++ utils/kms.utils.js
@@ -1,193 +1,196 @@
-const axios = require('axios');
-const env = process.env;
-axios.defaults.headers.common['access-token'] = env.KMS_TOKEN;
+const axios = require("axios");
+const { KMSEncrypt, KMSDecrypt } = require("../services/encryption.service");
 
 async function kmsConnection() {
-    const uri = `${env.KMS_URI}keys/`;
-    try {
-        return await axios.get(uri, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.log("Could not connect to KMS");
-    }
+  const uri = `${process.env.KMS_URI}keys/`;
+  try {
+    return await axios.get(uri, {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.log("Could not connect to KMS");
+  }
 }
 
 async function insertSignature(id, publicKey, privateKey, iv) {
-    const keyObj = {
-        _id: id,
-        public_key: publicKey,
-        private_key: privateKey,
-        iv: iv
-    };
-    const uri = `${env.KMS_URI}keys/user`;
-    try {
-        return await axios.post(uri, keyObj, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const keyObj = {
+    _id: id,
+    public_key: publicKey,
+    private_key: privateKey,
+    iv: iv,
+  };
+  const uri = `${process.env.KMS_URI}keys/user`;
+  try {
+    return await axios.post(uri, KMSEncrypt(JSON.stringify(keyObj)), {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function insertElectionKeys(id, publicKey, privateKey, iv) {
-    const keyObj = {
-        _id: id,
-        public_key: publicKey,
-        private_key: privateKey,
-        iv: iv
-    };
-    const uri = `${env.KMS_URI}keys/election`;
-    try {
-        return await axios.post(uri, keyObj, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const keyObj = {
+    _id: id,
+    public_key: publicKey,
+    private_key: privateKey,
+    iv: iv,
+  };
+  const uri = `${process.env.KMS_URI}keys/election`;
+  try {
+    return await axios.post(uri, KMSEncrypt(JSON.stringify(keyObj)), {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function getElectionPublicKey(id) {
-    const uri = `${env.KMS_URI}keys/election/public/${id}`;
-    try {
-        return await axios.get(uri, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/election/public/${id}`;
+  try {
+    const key = await axios.get(uri, {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+    return JSON.parse(KMSDecrypt(key));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function getElectionPrivateKey(id) {
-    const uri = `${env.KMS_URI}keys/election/private/${id}`;
-    try {
-        return await axios.get(uri, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/election/private/${id}`;
+  try {
+    const key = await axios.get(uri, {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+    return JSON.parse(KMSDecrypt(key));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function getSignaturePrivateKey(id) {
-    const uri = `${env.KMS_URI}keys/user/private/${id}`;
-    try {
-        return await axios.get(uri, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/user/private/${id}`;
+  try {
+    const key = await axios.get(uri, {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+    return JSON.parse(KMSDecrypt(key));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function getSignaturePublicKey(id) {
-    const uri = `${env.KMS_URI}keys/user/public/${id}`;
-    try {
-        return await axios.get(uri, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/user/public/${id}`;
+  try {
+    const key = await axios.get(uri, {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+    return JSON.parse(KMSDecrypt(key));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function updateElectionKeys(id, publicKey, privateKey, iv) {
-    const uri = `${env.KMS_URI}keys/election/${id}`;
-    const keyObj = {
-        public_key: publicKey,
-        private_key: privateKey,
-        iv: iv
-    };
-    try {
-        return await axios.patch(uri, keyObj, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/election/${id}`;
+  const keyObj = {
+    public_key: publicKey,
+    private_key: privateKey,
+    iv: iv,
+  };
+  try {
+    return await axios.patch(uri, KMSEncrypt(JSON.stringify(keyObj)), {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function updateSignatureKeys(id, publicKey, privateKey, iv) {
-    const uri = `${env.KMS_URI}keys/user/${id}`;
-    const keyObj = {
-        public_key: publicKey,
-        private_key: privateKey,
-        iv: iv
-    };
-    try {
-        return await axios.patch(uri, keyObj, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/user/${id}`;
+  const keyObj = {
+    public_key: publicKey,
+    private_key: privateKey,
+    iv: iv,
+  };
+  try {
+    return await axios.patch(uri, KMSEncrypt(JSON.stringify(keyObj)), {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function deleteElectionKeys(id) {
-    const uri = `${env.KMS_URI}keys/election/${id}`;
-    try {
-        return await axios.delete(uri, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/election/${id}`;
+  try {
+    return await axios.delete(uri, {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function deleteSignatureKeys(id) {
-    const uri = `${env.KMS_URI}keys/user/${id}`;
-    try {
-        return await axios.delete(uri, {
-            headers: {
-                "access-token": env.KMS_TOKEN,
-                "Content-type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  const uri = `${process.env.KMS_URI}keys/user/${id}`;
+  try {
+    return await axios.delete(uri, {
+      headers: {
+        "access-token": process.env.KMS_TOKEN,
+        "Content-type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = {
-    kmsConnection,
-    insertSignature,
-    insertElectionKeys,
-    getElectionPublicKey,
-    getElectionPrivateKey,
-    getSignaturePrivateKey,
-    getSignaturePublicKey,
-    deleteElectionKeys,
-    deleteSignatureKeys,
-    updateElectionKeys,
-    updateSignatureKeys
-}
+  kmsConnection,
+  insertSignature,
+  insertElectionKeys,
+  getElectionPublicKey,
+  getElectionPrivateKey,
+  getSignaturePrivateKey,
+  getSignaturePublicKey,
+  deleteElectionKeys,
+  deleteSignatureKeys,
+  updateElectionKeys,
+  updateSignatureKeys,
+};
